fd12c27b8910617a6c86695225bc28a0a78eb01d
Steve Jarvis <sajarvis@bu.edu>
2022-12-26 13:03:01 -0600

    update to node16, split policy statements, lint

--- disable-services/app.js
+++ disable-services/app.js
@@ -1,29 +1,29 @@
-const AWS = require('aws-sdk')
+const AWS = require('aws-sdk');
 
-const cloudfront = new AWS.CloudFront()
-const apigateway = new AWS.APIGateway()
+const cloudfront = new AWS.CloudFront();
+const apigateway = new AWS.APIGateway();
 
 const disableDistribution = async(id) => {
-  const config = await cloudfront.getDistributionConfig({ Id: id }).promise()
-  const disabledConfig = { ...config.DistributionConfig, Enabled: false }
+  const config = await cloudfront.getDistributionConfig({ Id: id }).promise();
+  const disabledConfig = { ...config.DistributionConfig, Enabled: false };
 
   await cloudfront.updateDistribution({
     Id: id,
     DistributionConfig: disabledConfig,
     IfMatch: config.ETag
-  }).promise()
-}
+  }).promise();
+};
 
 const disableCloudfront = async() => {
-  const distributions = await cloudfront.listDistributions().promise()
-  await Promise.all(distributions.DistributionList.Items.map(d => disableDistribution(d.Id)))
-}
+  const distributions = await cloudfront.listDistributions().promise();
+  await Promise.all(distributions.DistributionList.Items.map(d => disableDistribution(d.Id)));
+};
 
 const setStageLimits = async(restApiId, stageName) => {
   const stage = await apigateway.getStage({
     restApiId,
     stageName,
-  }).promise()
+  }).promise();
 
   await apigateway.updateStage({
     restApiId,
@@ -39,25 +39,24 @@ const setStageLimits = async(restApiId, stageName) => {
         value: '0'
       },
     ]
-  }).promise()
+  }).promise();
 
-  console.log(stage)
-}
+  console.log(stage);
+};
 
 const disableApiGateways = async() => {
-  const apiIds = (await apigateway.getRestApis().promise()).items.map(a => a.id)
+  const apiIds = (await apigateway.getRestApis().promise()).items.map(a => a.id);
 
   await Promise.all(apiIds.map(async(restApiId) => {
     const stages = (await apigateway.getStages({
       restApiId
-    }).promise()).item
+    }).promise()).item;
 
-    await Promise.all(stages.map(stage => setStageLimits(restApiId, stage.stageName)))
+    await Promise.all(stages.map(stage => setStageLimits(restApiId, stage.stageName)));
   }))
-
-}
+};
 
 exports.handler = async(event) => {
-  await disableCloudfront()
-  await disableApiGateways()
+  await disableCloudfront();
+  await disableApiGateways();
 };
